9d521f79d76b9f976383ccbd8073f211e5ebe38b
liuyang <liuyang2@pset.sentec.net>
2014-12-05 13:44:48 +0800

    modify the link_method.c

--- link_method.c
+++ link_method.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <sddlib.h>
+#include <stdlib.h>
 
 typedef struct intnode
 {
@@ -7,8 +7,8 @@ typedef struct intnode
 	struct intnode *next;
 }INT;
 
-
-INT* my_malloc()
+//内存分配函数
+INT* my_malloc()       
 {
     INT *tmp = (INT*)malloc(sizeof(INT));
 	tmp->next = NULL;
@@ -16,21 +16,69 @@ INT* my_malloc()
 	return tmp;
 }
 
+//节点初始化函数
+int inti_node(INT *node, int num)
+{
+    if(NULL == node){
+	    return 1;
+	}
 
+    node->num = num;
+
+	return 0;
+}
+
+//插入链表操作
 int insertlink(INT *head, INT *node)
 {
+    if((NULL == head) || (NULL == node) ){
+	    return 1;
+	}
+
     node->next = head->next;
     head->next = node;
 
-	return 1;
+	return 0;
 }
 
-
+//阶乘功能函数
 int func(int n)
 {
+    int i;                     //循环控制变量
+	INT *tmp = NULL;
+
     INT *head = my_malloc;
     
-    INT *fist = my_malloc;
+    INT *first = my_malloc;
+	inti_node(first, 1);
+    insertlink(head, node);
+
+	for(i=2; i<=n; i++)
+	{
+        tmp = head->next;
+	    while(NULL == tmp)
+		{
+		    tmp->num = tmp->num * i;
+			tmp = tmp-<next;
+		}
+
+        tmp = head->next;
+		while(NULL == tmp)
+		{
+		    if(tmp->num >= 10){
+			    if(NULL == tmp->next)
+				{
+				    tmp->next = my_malloc();
+				}
+
+                tmp->next->num = tmp->num / 10;
+				tmp->num = tmp->num % 10;
+			}
+
+			tmp = tmp->next;
+		}
+	}
+ 
 }
 
 
